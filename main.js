db9b246599f3e090c503e0cc5207d1709dcce6bd
Aleksey <ivrin567@mail.ru>
2020-11-30 15:09:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var myMap;
+ymaps.ready(init);
+
+function init() {
+	// Поиск координат центра Нижнего Новгорода.
+	ymaps.geocode('Москва', { results: 1 }).then(function (res) {
+		// Выбираем первый результат геокодирования.
+		var firstGeoObject = res.geoObjects.get(0),
+			// Создаем карту с нужным центром.
+			myMap = new ymaps.Map("map", {
+				center: [55.755814, 37.617635],
+				zoom: 10,
+				behaviors: ["default", "scrollZoom"]
+			});
+
+		myMap.container.fitToViewport();
+		attachReverseGeocode(myMap);
+	}, function (err) {
+		// Если геокодирование не удалось, сообщаем об ошибке.
+		alert(err.message);
+	});
+	// При щелчке левой кнопкой мыши выводится
+	// информация о месте, на котором щёлкнули.
+
+
+	function attachReverseGeocode(myMap) {
+		myMap.events.add('click', function (e) {
+			var coords = e.get('coordPosition');
+
+			// Отправим запрос на геокодирование.
+			ymaps.geocode(coords).then(function (res) {
+				var names = [];
+				// Переберём все найденные результаты и
+				// запишем имена найденный объектов в массив names.
+				res.geoObjects.each(function (obj) {
+					names.push(obj.properties.get('name'));
+				});
+				// Добавим на карту метку в точку, по координатам
+				// которой запрашивали обратное геокодирование.
+				myMap.geoObjects.add(new ymaps.Placemark(coords, {
+					// В качестве контента иконки выведем
+					// первый найденный объект.
+					iconContent: names[0],
+					// А в качестве контента балуна - подробности:
+					// имена всех остальных найденных объектов.
+					balloonContent: names.reverse().join(', ')
+				}, {
+					preset: 'twirl#redStretchyIcon',
+					balloonMaxWidth: '250'
+				}));
+			});
+		});
+	}
+
+}
